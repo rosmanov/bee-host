e05d2c1a564adb52ccb11759c324798fbaa14cf5
Ruslan Osmanov <rrosmanov@gmail.com>
2019-01-16 19:49:44 +0300

    Renamed json-replace to json-patch

--- install.sh
+++ install.sh
@@ -39,11 +39,11 @@ json_patch="{\"path\":\"$target_path\"}"
 tmp_manifest_file='tmp-manifest.json'
 
 target_manifest_path="$chrome_target_manifest_dir/$target_manifest_file"
-./json-replace "$dir/$chrome_manifest_file" "$json_patch" > "$tmp_manifest_file" && \
+./json-patch "$dir/$chrome_manifest_file" "$json_patch" > "$tmp_manifest_file" && \
 install -D -m 0644 "$tmp_manifest_file" "$target_manifest_path" && \
   printf "Installed Chrome manifest into '%s'\n" "$target_manifest_path"
 
 target_manifest_path="$firefox_target_manifest_dir/$target_manifest_file"
-./json-replace "$dir/$firefox_manifest_file" "$json_patch" > "$tmp_manifest_file" && \
+./json-patch "$dir/$firefox_manifest_file" "$json_patch" > "$tmp_manifest_file" && \
 install -D -m 0644 "$tmp_manifest_file" "$target_manifest_path" && \
   printf "Installed Firefox manifest into '%s'\n" "$target_manifest_path"
