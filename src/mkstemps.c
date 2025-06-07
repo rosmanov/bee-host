cffb96a9c34e93193d18494c09f42c8823a37d47
Ruslan Osmanov <608192+rosmanov@users.noreply.github.com>
2025-06-07 21:56:41 +0100

    Windows-compatible entropy in mkstemps

--- src/mkstemps.c
+++ src/mkstemps.c
@@ -4,6 +4,9 @@
 * Based on https://github.com/ClickHouse/ClickHouse/blob/3203fa4c34ac66990393e846621c89352fd4ac42/base/glibc-compatibility/musl/mkstemps.c
 * Original code was wrapped in an #ifndef preprocessor block.
 *
+* Modified by Ruslan Osmanov in 2025 to remove dependency on clock_gettime
+* and support Windows-compatible entropy source (GetSystemTimeAsFileTime).
+*
 * Copyright 2016-2021 Yandex LLC
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,18 +30,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
+#ifndef _WIN32
+# include <unistd.h>
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#include <stdint.h>
+
+static uint64_t win_time_entropy()
+{
+    FILETIME ft;
+    GetSystemTimeAsFileTime(&ft);
+    return ((uint64_t)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
+}
+#endif
 
 /* This assumes that a check for the
    template size has already been made */
 static char * __randname(char * template)
 {
     int i;
-    struct timespec ts;
     unsigned long r;
 
+#ifdef _WIN32
+    r = win_time_entropy();
+#else
+    struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
     r = (ts.tv_nsec * 65537) ^ ((((intptr_t)(&ts)) / 16) + ((intptr_t)template));
+#endif
     for (i = 0; i < 6; i++, r >>= 5)
         template[i] = 'A' + (r & 15) + (r & 16) * 2;
 
