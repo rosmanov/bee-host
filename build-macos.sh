d9d682ff967eb628ec48fcb99c57b002de3e828b
Ruslan Osmanov <608192+rosmanov@users.noreply.github.com>
2025-06-07 19:45:53 +0100

    Add scripts to build macOS x86_64 and arm64

--- build-macos.sh
+++ build-macos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -
 
 project_dir="$(dirname "$0")"
-"$project_dir"/build.sh "$project_dir"/CMake/Toolchain-macos.cmake "$@"
+"$project_dir"/build.sh "$project_dir"/CMake/Toolchain-macos-arm64.cmake "$@"
+"$project_dir"/build.sh "$project_dir"/CMake/Toolchain-macos-x86_64.cmake "$@"
